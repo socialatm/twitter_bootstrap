fbac928b25881cab01834712d218ac4d2f6a6eb6
ray peaslee <github@twentyfiveautumn.com>
2012-10-11 20:50:22 -0400

    Profile page
    more scaffolding... this page was a bitch...

--- views/default/profile/details.php
+++ views/default/profile/details.php
@@ -9,7 +9,7 @@ $user = elgg_get_page_owner_entity();
 $profile_fields = elgg_get_config('profile_fields');
 
 //twitter bootstrap
-echo '<div id="profile-details" class="span5">';
+echo '<div id="profile-details" class="span9">';
 echo "<h2>{$user->name}</h2>";
 
 echo elgg_view("profile/status", array("entity" => $user));
