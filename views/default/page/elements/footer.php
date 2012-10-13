111bde7897cf2d2d4c03ab87a55cc007605848f9
ray peaslee <github@twentyfiveautumn.com>
2012-10-13 10:42:44 -0400

    Footer changes
    A bit of re-wording and commented out the default Ell powered by graphic
    for now...

--- views/default/page/elements/footer.php
+++ views/default/page/elements/footer.php
@@ -15,20 +15,21 @@ $powered_url = elgg_get_site_url() . "_graphics/powered_by_elgg_badge_drk_bckgnd
 
 echo '<div class="span6">';
 echo <<<HTML
-
-Twitter Bootstrap for Elgg is made possible by the awesome <a href="http://elgg.org">Elgg engine</a> and 
-impressive <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a>.
-
+Powered by the <a href="http://elgg.org">Elgg engine</a> and <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a>.
 HTML;
 
 echo '</div><!-- /span6 -->';
 
 echo '<div class="mts clearfloat float-alt span4 offset2">';
 echo elgg_view_menu('footer', array('sort_by' => 'priority', 'class' => 'elgg-menu-hz pull-right'));
+
+/*
 echo elgg_view('output/url', array(
 	'href' => 'http://elgg.org',
 	'text' => "<img src=\"$powered_url\" alt=\"Powered by Elgg\" width=\"106\" height=\"15\" />",
 	'class' => 'pull-right',
 	'is_trusted' => true,
 ));
+*/
+
 echo '</div>';
