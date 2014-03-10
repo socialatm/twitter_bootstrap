b74fa68da544bb95e3602dcef04b69f3c4f73cca
ray peaslee <github@twentyfiveautumn.com>
2014-03-09 20:23:40 -0400

    remove outdated functions
    add the youDecide code and the wysihtml5

--- views/default/twitter_bootstrap/custom.js
+++ views/default/twitter_bootstrap/custom.js
@@ -48,38 +48,66 @@ $(document).ready(function () {
 	bootstrapMapCss('form#elgg-form-members-search .elgg-input-radios li', '', 'radio inline');	// inline radio buttons on the members page form... twitter bootstrap css
 
 /* end replace Elgg css classes with Twitter bootstrap css classes	*/
-/* set minimum height of content to force the footer to the bottom of the page	*/
-	var tb_content = $('#tb-content').height();
-	if (tb_content < 700) {$('#tb-content').height(700); }
 	
 /*	don't show the river comment form unless the user is logged in	*/
 	if(!elgg.is_logged_in()) {
 		$('div.elgg-river-responses form.form-horizontal').hide();
 		$('.elgg-river-comments li:last-child').css('border-bottom', '1px solid #D4D4D4');
 	};
-/*	adjust label with on forms when user is not logged in	*/
-
-if(!elgg.is_logged_in()) {
-		$('.control-label').css('width', '75');
-	};
 	
 	$('#elgg-river-selector').addClass('pull-right');
 	
-/*	removes the placeholder on focus, adds it back on blur	*/
-
-$(".elgg-river-item input.controls").focus(function() {
-  $(this).attr('placeholder', '' );
-});
-$(".elgg-river-item input.controls").blur(function() {
-  $(this).attr('placeholder', elgg.echo("twitter_bootstrap:addcomments") );
-});
+	/*	removes the placeholder on focus, adds it back on blur	*/
+	$("div.elgg-river-responses input.controls-text").focus(function () {
+		$(this).attr('placeholder', '');
+	});
+	$("div.elgg-river-responses input.controls-text").blur(function () {
+		$(this).attr('placeholder', elgg.echo("twitter_bootstrap:addcomments"));
+	});
 
 /*	the fix for combining the search forms on the members page	*/
-
 	$('form#elgg-form-members-search input:radio').change(function () {
 		var action = elgg.config.wwwroot + 'members/search/';
-		$('form#elgg-form-members-search').attr('action' , action + $(this).val() );
-		$('.search-query').attr('name' , $(this).val() );
+		$('form#elgg-form-members-search').attr('action', action + $(this).val());
+		$('.search-query').attr('name', $(this).val());
 	});
 	
+/*	the you decide button	*/
+	$(".elgg-input-access:checked").parent().addClass('btn-success');
+	$('div.dropdown-menu label input.elgg-input-access:checked').parent().parent().siblings('.dropdown-toggle').addClass('btn-success');
+
+	$(".elgg-input-access").parent().click(function () {
+		$(this).siblings('#read-access-caret').removeClass('btn-success');
+		$(this).siblings().children(".elgg-input-access:checked").parent().removeClass('btn-success');
+		$(this).siblings().children(".elgg-input-access:checked").removeAttr('checked');
+		$(this).siblings('.dropdown-menu').children('.btn-success').removeClass('btn-success');
+		$(this).siblings('.dropdown-menu').children().children(".elgg-input-access:checked").removeAttr('checked');
+		$(this).parent().siblings('.btn-success').children().removeAttr('checked');
+		$(this).parent().siblings('.btn-success').removeClass('btn-success');
+		$(this).parent().siblings('#read-access-caret').addClass('btn-success');
+		var value = $(this).children().attr('checked', 'checked');
+		$(this).addClass('btn-success');
+	});
+
+/*****	bootstrap-wysihtml5	*****/
+
+	$('textarea').wysihtml5();
+	editor.composer.iframe.scrolling = "no";
+	editor.composer.iframe.style.maxHeight = "260px";
+	var larger = "260px";
+
+	editor.observe("load", function () {
+		$(editor.composer.element).focus(function () {
+			$(editor.composer.iframe).height(larger);
+			editor.composer.iframe.height = larger;
+		});
+
+		$(editor.composer.element).blur(function () {
+			if (editor.composer.iframe.height === larger) {$(editor.composer.iframe).height(larger); }
+		});	// end blur
+
+		$("#access_id_btn").click(function () {
+			$(this).focus();
+		});
+	});	// end observe
 });
