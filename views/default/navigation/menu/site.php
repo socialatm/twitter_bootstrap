c277084dbb6b032c361889bfaf211bbc5adacd7b
ray peaslee <github@twentyfiveautumn.com>
2014-03-17 18:17:56 -0400

    site menu fixed

--- views/default/navigation/menu/site.php
+++ views/default/navigation/menu/site.php
@@ -8,42 +8,58 @@
 
 $default_items = elgg_extract('default', $vars['menu'], array());
 $more_items = elgg_extract('more', $vars['menu'], array());
+$search = elgg_echo('search');
+$more = elgg_echo('more');
 
-echo <<<HTML
-	<div class="navbar">
-    <div class="">
-    <ul class="nav">
-HTML;
+/***** start featured menu	*****/
+$featured_menu = '';
 foreach ($default_items as $menu_item) {
-	echo elgg_view('navigation/menu/elements/item', array('item' => $menu_item)).'<li class=""></li>';
+	$featured_menu .= elgg_view('navigation/menu/elements/item', array('item' => $menu_item));
 }
-if ($more_items) {
-	echo '<li class="elgg-more dropdown">';
-	$more = elgg_echo('more');
-	echo '<a class="dropdown-toggle" data-toggle="dropdown" href="#">'.$more .' <b class="caret white"></b></a>';
-	echo elgg_view('navigation/menu/elements/section', array(
-		'class' => 'elgg-menu elgg-menu-site elgg-menu-site-more dropdown-menu', 
+/*****	END featured menu	*****/
+
+/*****	start more menu	*****/
+$more_menu = '';
+if ($more_items){
+	$more_menu .= elgg_view('navigation/menu/elements/section', array(
+		'class' => 'dropdown-menu', 
 		'items' => $more_items,
-	));
-	echo '</li>';
+		));
 	}
-	
-	$search = elgg_echo('search');
-	
-echo '</ul><form class="navbar-search pull-right" action="'.elgg_get_site_url().'search" method="get">';
-echo <<<HTML
-	<div class="input-group">
-    <input type="text" class="search-query" placeholder="" name="q">
-	<button type="submit" class="btn btn-default">{$search}</button>
-	</div>
-	<input type="hidden" name="search_type" value="all" />
-    </form>
-	</div>
-    </div>
-HTML;
+/*****	End more menu	*****/
 
+/*****	new site navbar	*****/
 
-    
-
-
-
+$site_navbar = '<!-- Static navbar -->
+	<div class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					'.$featured_menu.'
+					<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">'.$more.' <b class="caret"></b></a>
+						'.$more_menu.'
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<form class="navbar-form navbar-left" role="search" action="'.elgg_get_site_url().'search" method="get">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="'.$search.'" name="q" />
+						</div>
+						<button type="submit" class="btn btn-default">'.$search.'</button>
+						<input type="hidden" name="search_type" value="all" />
+					</form>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div><!--/.container-fluid -->
+	</div>';
+	  
+echo $site_navbar;
