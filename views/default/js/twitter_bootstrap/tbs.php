8d41cbebbc2d2681b16e2f8bbbfd15db572b06ab
ray peaslee <github@twentyfiveautumn.com>
2014-03-18 18:14:36 -0400

    system messages
    display success message for 3 seconds and then remove

--- views/default/js/twitter_bootstrap/tbs.php
+++ views/default/js/twitter_bootstrap/tbs.php
@@ -5,9 +5,9 @@
 ?>
 elgg.provide('elgg.tbs');
 
-
 elgg.tbs.init = function() {
-	
+
+	$(".alert-success").delay(3000).slideUp("slow", function(){$(this).remove();});	//	delays a success message for 3 seconds then removes it
 };
 
 elgg.register_hook_handler('init', 'system', elgg.tbs.init);
@@ -15,3 +15,6 @@ elgg.register_hook_handler('init', 'system', elgg.tbs.init);
 //	@todo - move below info into elgg.tbs namespace
 
 	alert('tbs is loaded');
+	
+	
+
