90c35d89eb0bd61e88afebc0b35b8458b936538a
ray peaslee <github@twentyfiveautumn.com>
2014-09-17 19:27:19 -0400

    correct the styling of system error messages

--- views/default/page/elements/messages.php
+++ views/default/page/elements/messages.php
@@ -12,7 +12,7 @@
 if (isset($vars['object']) && is_array($vars['object']) && sizeof($vars['object']) > 0) {
 	foreach ($vars['object'] as $type => $list ) {
 		if(!empty($list)) {
-	
+		$type = ($type === 'error')? 'danger':$type; 
 		echo '<div class="alert alert-'.$type.' bootstrap-messages col-md-12">';
 		echo '<a class="close" data-dismiss="alert" href="#">&times;</a>';
 			foreach ($list as $message) {
