a5dee97d5a149977ae4e8998d34b76d89434e216
ray peaslee <github@twentyfiveautumn.com>
2014-09-18 15:55:11 -0400

    use bundled in jQuery
    we'll make a plugin setting so this is optional later. Issue opened in
    github

--- start.php
+++ start.php
@@ -7,12 +7,6 @@ function twitter_bootstrap_init() {
 
 	//include twitter bootstrap css
 	elgg_extend_view('css/elgg', 'twitter_bootstrap/css');
-
-	
-	
-	//we use google jquery instead of Elgg's as it is more up-to-date and required for bootstrap
-	$google_jquery = 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js';
-	elgg_register_js('jquery', $google_jquery);
 	
 	//register bootstrap css and js
 	$bootstrap_js = 'mod/twitter_bootstrap/vendors/bootstrap/js/bootstrap.min.js';
