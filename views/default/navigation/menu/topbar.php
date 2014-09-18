b0e23b4b60a23b32e7903474bf583cebb7e26ecb
ray peaslee <github@twentyfiveautumn.com>
2014-09-18 12:32:43 -0400

    closes issue 11

--- views/default/navigation/menu/topbar.php
+++ views/default/navigation/menu/topbar.php
@@ -58,20 +58,6 @@ $new_topbar = '<div class="navbar navbar-inverse navbar-fixed-top" role="navigat
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
 			'.$topbar_main_menu.'
-
-<!-- leave this dropdown until we get the chat menu done	-->			
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
