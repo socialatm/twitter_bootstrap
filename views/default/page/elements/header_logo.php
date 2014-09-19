0afd9a6cb110792429f4921809cccca178595092
ray peaslee <github@twentyfiveautumn.com>
2014-09-18 23:02:39 -0400

    fix link to logo image

--- views/default/page/elements/header_logo.php
+++ views/default/page/elements/header_logo.php
@@ -7,7 +7,7 @@ $site = elgg_get_site_entity();
 $site_name = $site->name;
 $site_description = $site->description;
 $site_url = elgg_get_site_url();
-$logo = '<img src="mod/twitter_bootstrap/responsive.png" alt="'.$site_name.'" class="img-responsive" id="tbs_logo" />';
+$logo = '<img src="'.$site_url.'mod/twitter_bootstrap/responsive.png" alt="'.$site_name.'" class="img-responsive" id="tbs_logo" />';
 ?>
 
 <h1>
