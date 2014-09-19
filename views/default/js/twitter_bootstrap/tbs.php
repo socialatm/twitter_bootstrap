e56ed62f01efc91d4526d429bacfaa5e56622bd2
ray peaslee <github@twentyfiveautumn.com>
2014-09-19 12:27:35 -0400

    Likes popup
    fixes  who likes this popup not working #12

--- views/default/js/twitter_bootstrap/tbs.php
+++ views/default/js/twitter_bootstrap/tbs.php
@@ -25,7 +25,18 @@ elgg.tbs.init = function() {
 $(".elgg-menu-item-likes-count a").tooltip({
 	container: 'body',
 });
-	
+
+/*****	likes popover	*****/
+
+$(".elgg-menu-item-likes-count a").popover({
+	container: 'body',
+	placement: 'top',
+	html: true,
+	content: function(){
+	return $(this).parent().children(".elgg-likes").html();
+	},
+});
+
 };		//	end elgg.tbs.init
 
 elgg.register_hook_handler('init', 'system', elgg.tbs.init);
