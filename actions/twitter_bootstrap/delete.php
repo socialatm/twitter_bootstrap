16711241c1f0c49fb31a661c202cc45495d798b6
ray peaslee <github@twentyfiveautumn.com>
2015-02-11 09:55:04 -0500

    add forward(REFERER) on error
    don't let user delete if they don't belong here

--- actions/twitter_bootstrap/delete.php
+++ actions/twitter_bootstrap/delete.php
@@ -8,28 +8,19 @@
 require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))) . "/engine/start.php");
 
 $guid = (int) get_input('guid');
-
 $file = new FilePluginFile($guid);
 if (!$file->guid) {
 	register_error(elgg_echo("file:deletefailed"));
-	forward('file/all');
+	forward(REFERER);
 }
 
 if (!$file->canEdit()) {
 	register_error(elgg_echo("file:deletefailed"));
-	forward($file->getURL());
+	forward(REFERER);
 }
-
-$container = $file->getContainerEntity();
 
 if (!$file->delete()) {
 	register_error(elgg_echo("file:deletefailed"));
 } else {
 	system_message(elgg_echo("file:deleted"));
-}
-
-if (elgg_instanceof($container, 'group')) {
-	forward("file/group/$container->guid/all");
-} else {
-	forward("file/owner/$container->username");
 }
