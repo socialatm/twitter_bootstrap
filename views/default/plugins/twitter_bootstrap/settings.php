2486432382d09ee60f1a45cbd221c1b12a2abd0c
ray peaslee <github@twentyfiveautumn.com>
2015-03-02 11:20:24 -0500

    add the my status settings to bootstrap

--- views/default/plugins/twitter_bootstrap/settings.php
+++ views/default/plugins/twitter_bootstrap/settings.php
@@ -63,3 +63,51 @@ echo elgg_view('input/select', array(
 	'value' => $vars['entity']->require_email_login,
 ));
 echo '</div>';
+
+/*****	My Status settings	*****/
+
+	if (!isset($vars['entity']->num_entries)) {
+		$vars['entity']->num_entries = 10;
+	}
+	if (!isset($vars['entity']->river_post)) {
+		$vars['entity']->river_post = 'yes';
+	}
+	
+echo '<div class="elgg-module-main">';
+echo '<h3>'.elgg_echo('my:status:num_entries').'</h3>';
+?>
+    <select name='params[num_entries]'>
+		<option value="5"  <?php if ($vars['entity']->num_entries == 5 ) echo ' selected="yes" '; ?>>5</option>
+		<option value="10" <?php if ($vars['entity']->num_entries == 10) echo ' selected="yes" '; ?>>10</option>
+		<option value="15" <?php if ($vars['entity']->num_entries == 15) echo ' selected="yes" '; ?>>15</option>
+		<option value="20" <?php if ($vars['entity']->num_entries == 20) echo ' selected="yes" '; ?>>20</option>
+		<option value="25" <?php if ($vars['entity']->num_entries == 25) echo ' selected="yes" '; ?>>25</option>
+		<option value="30" <?php if ($vars['entity']->num_entries == 30) echo ' selected="yes" '; ?>>30</option>
+    </select>
+
+	</div>
+	<div class="elgg-module-main">
+<?php
+
+echo '<h3>'.elgg_echo('my:status:river:post').'</h3>';
+
+echo elgg_view('input/dropdown', array(
+	'name' => 'params[river_post]',
+	'options_values' => array(
+		'yes' => elgg_echo('option:yes'),
+		'no' => elgg_echo('option:no')
+	),
+	'value' => $vars['entity']->river_post,
+));
+echo '</div>';
+
+echo '<div class="elgg-module-main">';
+echo '<h3>Twitter Bootstrap Theme v1.10</h3>
+Programmers: Ray Peaslee<br />
+<a href="http://twentyfiveautumn.com/">twentyfiveautumn.com</a><br />
+License: <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU General Public License v2</a>
+</p>';
+echo '</div>';
+
+
+
