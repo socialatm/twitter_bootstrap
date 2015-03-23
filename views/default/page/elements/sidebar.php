8b08c5a04dcb469ed5314c784b3007c0fd2ba2db
ray peaslee <github@twentyfiveautumn.com>
2015-03-22 20:19:31 -0400

    added a switch statement to the sidebar
    so far, so good

--- views/default/page/elements/sidebar.php
+++ views/default/page/elements/sidebar.php
@@ -10,24 +10,36 @@ echo elgg_view_menu('extras', array(
 	'class' => 'elgg-menu-hz',
 ));
 
-if (elgg_get_context() == 'activity'){
-	$type = preg_replace('[\W]', '', get_input('type', 'all'));
-	$subtype = preg_replace('[\W]', '', get_input('subtype', ''));
-	if ($subtype) {
-		$selector = "type=$type&subtype=$subtype";
-	} else {
-		$selector = "type=$type";
-	}
+$context = elgg_get_context();
 
-	echo '<h3 class="media-heading">'.elgg_echo('activity:filter').'</h3>';
-	echo elgg_view('core/river/filter', array('selector' => $selector));
-}
+switch ($context) {
+    case 'profile':
+		$params = array(
+			'num_columns' => 1,
+		);
+		echo elgg_view_layout('widgets', $params);
+		break;
+    case 'activity':
+        $type = preg_replace('[\W]', '', get_input('type', 'all'));
+		$subtype = preg_replace('[\W]', '', get_input('subtype', ''));
+		if ($subtype) {
+			$selector = "type=$type&subtype=$subtype";
+		} else {
+			$selector = "type=$type";
+		}
+	
+		echo '<h3 class="media-heading">'.elgg_echo('activity:filter').'</h3>';
+		echo elgg_view('core/river/filter', array('selector' => $selector));
+		
+		echo elgg_view('page/elements/owner_block', $vars);
+		echo elgg_view_menu('page', array('sort_by' => 'name'));
+        break;
+	default:
+		echo elgg_view('page/elements/owner_block', $vars);
+		echo elgg_view_menu('page', array('sort_by' => 'name'));
 
-echo elgg_view('page/elements/owner_block', $vars);
-
-echo elgg_view_menu('page', array('sort_by' => 'name'));
-
-// optional 'sidebar' parameter
-if (isset($vars['sidebar'])) {
-	echo $vars['sidebar'];
+		// optional 'sidebar' parameter
+		if (isset($vars['sidebar'])) {
+			echo $vars['sidebar'];
+		}
 }
