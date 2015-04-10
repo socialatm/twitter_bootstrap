f79445da6a425fce1a27768393640a5ce7de5ab2
ray peaslee <github@twentyfiveautumn.com>
2015-04-10 19:40:23 -0400

    a pretty big screwup
    been mia since 1.8 wtf?

--- views/default/page/default.php
+++ views/default/page/default.php
@@ -39,6 +39,7 @@ $lang = get_current_language();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>" lang="<?php echo $lang; ?>">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php echo elgg_view('page/elements/head', $vars); ?>
 </head>
 <body>
