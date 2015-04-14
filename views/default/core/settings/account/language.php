a95b7c90f0c439a80d817ef094211518d5849416
ray peaslee <github@twentyfiveautumn.com>
2015-04-14 11:28:51 -0400

    updated to use input/selectlist

--- views/default/core/settings/account/language.php
+++ views/default/core/settings/account/language.php
@@ -9,21 +9,33 @@
 $user = elgg_get_page_owner_entity();
 
 if ($user) {
+
+	$lang = get_installed_translations()[$user->language];
 	$label = elgg_echo('user:language:label') . ': ';
-	$value = elgg_view("input/select", array(
-		'name' => 'language',
+	$value = elgg_view("input/selectlist", array(
+		'name' => 'language_ul',
 		'value' => $user->language,
 		'options_values' => get_installed_translations(),
-		'class' => 'form-control',
-		'id' => 'language'
+		'class' => 'dropdown-menu',
+		'id' => 'language_ul',
+		'role' => 'menu'
 	));
 	
 echo <<<EOT
-		<div class="form-group">
+	<div class="form-group">
 		<label for="language" class="col-sm-2 control-label">{$label}</label>
 		<div class="col-sm-10">
-			{$value}
+			<div id="selectlistIllustration" data-initialize="selectlist"  class="btn-group btn-block selectlist">
+				<button type="button" data-toggle="dropdown" class="btn btn-default btn-block dropdown-toggle" aria-expanded="false">
+					<span class="selected-label pull-left">{$lang}</span>
+					<span class="glyphicon glyphicon-triangle-bottom pull-left" aria-hidden="true" style="margin-left: 10px;"></span>
+					<span class="sr-only">Toggle Dropdown</span>
+				</button>
+				{$value}
+				<input type="text" aria-hidden="true" readonly="readonly" id="language" name="language" class="hidden hidden-field" />
+			</div>
 		</div>
 	</div>
 EOT;
+
 }
