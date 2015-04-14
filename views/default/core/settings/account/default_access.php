d8ef9c020ab208e0332abca2f8852981fad4359d
ray peaslee <github@twentyfiveautumn.com>
2015-04-14 17:18:18 -0400

    updates for using input/selectlist

--- views/default/core/settings/account/default_access.php
+++ views/default/core/settings/account/default_access.php
@@ -14,21 +14,34 @@ if (elgg_get_config('allow_user_default_access')) {
 			$default_access = elgg_get_config('default_access');
 		}
 
+		$user_guid = $user->guid;
 		$label = elgg_echo('default_access:label') . ': ';
 		$value = elgg_view("input/access", array(
-		'name' => 'default_access',
+		'name' => 'default_access_ul',
 		'value' => $default_access,
-		'class' => 'form-control',
-		'id' => 'default_access'
+		'class' => 'dropdown-menu',
+		'id' => 'default_access_ul',
+		'role' => 'menu'
 	));
 	
+	$access_label = get_write_access_array()["$default_access"];
+
 echo <<<EOT
-		<div class="form-group">
-		<label for="default_access" class="col-sm-2 control-label">{$label}</label>
+	<div class="form-group">
+		<label for="default-access-{$user_guid}" class="col-sm-2 control-label">{$label}</label>
 		<div class="col-sm-10">
-			{$value}
+			<div id="default-access-{$user_guid}" data-initialize="selectlist"  class="btn-group btn-block selectlist">
+				<button type="button" data-toggle="dropdown" class="btn btn-success btn-block dropdown-toggle" aria-expanded="false">
+					<span class="selected-label pull-left">{$access_label}</span>
+					<span class="glyphicon glyphicon-triangle-bottom pull-left" aria-hidden="true" style="margin-left: 10px;"></span>
+					<span class="sr-only">Toggle Dropdown</span>
+				</button>
+				{$value}
+				<input type="text" aria-hidden="true" readonly="readonly" id="default_access" name="default_access" class="hidden hidden-field" />
+			</div>
 		</div>
 	</div>
 EOT;
+
 	}
 }
--- views/default/core/settings/account/language.php
+++ views/default/core/settings/account/language.php
@@ -10,6 +10,7 @@ $user = elgg_get_page_owner_entity();
 
 if ($user) {
 
+	$user_guid = $user->guid;
 	$lang = get_installed_translations()[$user->language];
 	$label = elgg_echo('user:language:label') . ': ';
 	$value = elgg_view("input/selectlist", array(
@@ -23,9 +24,9 @@ if ($user) {
 	
 echo <<<EOT
 	<div class="form-group">
-		<label for="language" class="col-sm-2 control-label">{$label}</label>
+		<label for="language-{$user_guid}" class="col-sm-2 control-label">{$label}</label>
 		<div class="col-sm-10">
-			<div id="selectlistIllustration" data-initialize="selectlist"  class="btn-group btn-block selectlist">
+			<div id="language-{$user_guid}" data-initialize="selectlist"  class="btn-group btn-block selectlist">
 				<button type="button" data-toggle="dropdown" class="btn btn-default btn-block dropdown-toggle" aria-expanded="false">
 					<span class="selected-label pull-left">{$lang}</span>
 					<span class="glyphicon glyphicon-triangle-bottom pull-left" aria-hidden="true" style="margin-left: 10px;"></span>
